f93aa1b7b2054288e3217aeac0c61d487208de7b
Constantin <xconstruct@gmail.com>
2009-07-29 01:45:59 +0200

    removed cargOnlyChat, minimalistic interface ftw

--- cargFellowSoldiers/core.lua
+++ cargFellowSoldiers/core.lua
@@ -2,41 +2,57 @@
 	cargFellowSoldiers
 ]]
 local texturepath
+local dummy = function() end
 if(IsAddOnLoaded("cargFellowSoldiers")) then -- Loaded in cargInterface or externally?
 	texturepath = [[Interface\AddOns\cargFellowSoldiers\]]
 else
 	texturepath = [[Interface\AddOns\cargInterface\cargFellowSoldiers\]]
 end
+local blizzIcon = [[Interface\Worldmap\WorldMapPartyIcon]]
 
-local addon = CreateFrame"Frame"
 local friends = {}
-local _G = getfenv(0)
 for i=1, GetNumFriends() do
 	friends[GetFriendInfo(i)] = true
 end
+for i=1, GetNumGuildMembers(true) do
+	friends[GetGuildRosterInfo(i)] = true
+end
+local _G = getfenv(0)
+local setTex = WorldMapRaid1Icon.SetTexture
 
+local i=1
+while(_G['WorldMapRaid'..i]) do
+	local icon = _G['WorldMapRaid'..i..'Icon']
+	icon.SetVertexColor = dummy
+	icon.SetTexture = dummy
+	i = i+1
+end
+
+local addon = CreateFrame"Frame"
 addon:SetScript("OnEvent", function()
-	local blizzIcon = [[Interface\Worldmap\WorldMapPartyIcon]]
-	local name, server, icon
-
 	local colorServer = select(2, IsInInstance()) == "pvp"
-	for i=1, MAX_RAID_MEMBERS do
-		name, server = UnitName("raid"..i)
-		icon = _G['WorldMapRaid'..i..'Icon']
-		button = _G['WorldMapRaid'..i]
-		server = server == "" and nil or server
+	local i=1
+	while(_G['WorldMapRaid'..i]) do
+		local button = _G['WorldMapRaid'..i]
+		local unit = button.unit
+		if(unit) then
+			local name, server = UnitName(unit)
+			local icon = _G['WorldMapRaid'..i..'Icon']
+			server = server == "" and nil or server
 
-		if(icon and friends[name] and not server) then
-			icon:SetTexture(texturepath.."GreenDot")
-			button:SetFrameLevel(5)
-		elseif(icon and name and not server and colorServer) then
-			icon:SetTexture(texturepath.."BlueDot")
-			button:SetFrameLevel(4)
-		elseif(icon) then
-			icon:SetTexture(blizzIcon)
-			button:SetFrameLevel(3)
+			if(friends[name] and not server) then
+				setTex(icon, texturepath.."GreenDot")
+				button:SetFrameLevel(5)
+			elseif(name and not server and colorServer) then
+				setTex(icon, texturepath.."BlueDot")
+				button:SetFrameLevel(4)
+			else
+				setTex(icon, blizzIcon)
+				button:SetFrameLevel(3)
+			end
+			i = i+1
 		end
 	end
 end)
-addon:RegisterEvent"PLAYER_ENTERING_WORLD"
 addon:RegisterEvent"RAID_ROSTER_UPDATE"
+addon:RegisterEvent"PLAYER_ENTERING_WORLD"
