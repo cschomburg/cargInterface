2789aae74d9e25e03fa2c44eaba42af0e0e86cf6
Constantin <xconstruct@gmail.com>
2009-08-08 23:34:06 +0200

    yay, cleaner! removed picoEXP

--- cargBroker/core.lua
+++ cargBroker/core.lua
@@ -1,36 +1,34 @@
-local friends = cargoShip("picoFriends", { noIcon = true, })
+local friends = cargoShip{
+	name = "picoFriends",
+	noIcon = true
+}
 friends:SetPoint("BOTTOMRIGHT", cargUI2, "BOTTOMRIGHT", -5, 2)
 
-local durability = cargoShip("Attrition", { noIcon = true, })
+local durability = cargoShip{
+	name = "Attrition",
+	noIcon = true
+}
 durability:SetPoint("RIGHT", friends, "LEFT", -15, 0)
 
 local fps = cargoShip("picoFPS")
 fps:SetPoint("RIGHT", durability, "LEFT", -15, 0)
 
-local xp = cargoShip("picoEXP", {
-	noIcon = true,
-	scale = 1.4,
-	fontStyle = "OUTLINE",
-})
-xp:SetPoint("RIGHT", durability, "LEFT", -60, 0)
+local bugs = cargoShip("BugSack")
+bugs:SetPoint("RIGHT", durability, "LEFT", -90, 0)
 
-local bugs = cargoShip("BugSack", {})
-bugs:SetPoint("RIGHT", xp, "LEFT", -15, 0)
-
-local honor = cargoShip("cargoHonor", {
+local honor = cargoShip{
+	name = "cargoHonor",
 	scale = 1.7,
-	fontStyle = "OUTLINE",
-})
+	fontStyle = "OUTLINE"
+}
 honor:SetPoint("BOTTOM", 0, 10)
 
 local f = CreateFrame"Frame"
 f:SetScript("OnEvent", function(self, event)
 	if(select(2, IsInInstance()) == "pvp") then
 		honor:Show()
-		xp:Hide()
 	else
 		honor:Hide()
-		xp:Show()
 	end
 end)
 f:RegisterEvent"PLAYER_ENTERING_WORLD"
