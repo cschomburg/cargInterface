a215218a011e43b9a11751100b446a6b6408fb14
xconstruct <xconstruct@gmail.com>
2009-12-19 19:22:03 +0100

    quest log: display exp in percent

---
+++ questxp.lua
@@ -0,0 +1,8 @@
+function QuestInfoXPFramePoints:SetText(text)
+	local points = tonumber(text)
+	if(points) then
+		self:SetFormattedText("%.1f%%  -  %d", points/UnitXPMax("player")*100, points)
+	else
+		self:SetFormattedText(text)
+	end
+end
