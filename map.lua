cbc42df2e61f7a30d4f3eb9db0d3fce34f873a6f
xconstruct <xconstruct@gmail.com>
2010-06-12 20:46:48 +0200

    fixed map-scaling in combat

--- map.lua
+++ map.lua
@@ -1,16 +1,20 @@
---[[
-	cargMap
-]]
-
-local function tweak()
-	WorldMapFrame:SetScale(0.75)
-	WorldMapFrame:EnableKeyboard(false)
-	BlackoutWorld:Hide()
-	WorldMapFrame:EnableMouse(false)
-	UIPanelWindows["WorldMapFrame"].area = "center"
-	WorldMapFrame:SetAttribute("UIPanelLayout-defined", false);
+-- Scaling fixes, needed since WorldMapFrame became tainted
+local _SetupFullScreenScale = SetupFullScreenScale
+function SetupFullscreenScale(self)
+	if(self ~= WorldMapFrame) then
+		return _SetupFullscreenScale(self)
+	end
 end
 
-hooksecurefunc(WorldMapFrame, "Show", tweak)
+local function tweak()
+	BlackoutWorld:Hide()
+	WorldMapFrame:EnableKeyboard(nil)
+	WorldMapFrame:EnableMouse(nil)
+	WorldMapFrame:SetScale(0.75)
+
+	UIPanelWindows["WorldMapFrame"].area = "center"
+	WorldMapFrame:SetAttribute("UIPanelLayout-defined", nil)
+end
+
 hooksecurefunc("WorldMap_ToggleSizeUp", tweak)
 tweak()
